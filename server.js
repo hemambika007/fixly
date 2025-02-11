521790ed10d1bbd8bf613e560d7ec985057ac022
hemambika007 <101657680+hemambika007@users.noreply.github.com>
2025-02-11 21:28:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
-app.use(cors());
-app.use(express.json()); // To handle JSON requests
 
-// Import and use routes
+app.use(cors());
+app.use(express.json()); 
+
+
 const apiRoutes = require("./routes/apiRoutes");
 app.use("/api", apiRoutes);  // All routes will start with "/api"
 
